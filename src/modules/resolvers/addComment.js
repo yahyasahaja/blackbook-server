e9389ea1522a5572e0c58a98e5be3b5757749740
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-11-07 19:13:29 +0700

    update comment

--- src/modules/resolvers/addComment.js
+++ src/modules/resolvers/addComment.js
@@ -1,14 +1,14 @@
 import db from '../../models'
 // import { JWT, USER_TYPE } from '../../config'
 
-export default async (obj, { heroId, comment, video_url, image_url }, { scope }) => {
+export default async (obj, { heroId, comment, video_url, image_url }, { scope, user }) => {
   if (scope.includes('addComment')) {
     try {
       let hero = await db.models.Hero.findById(heroId)
 
       if (!hero) throw new Error('Hero id is not exist')
-
-      return await hero.addComment({ comment, video_url, image_url })
+      
+      return await db.models.Comment.create({ hero_id: heroId, user_id: user.id, comment, video_url, image_url })
     } catch (error) {
       throw error
     }
