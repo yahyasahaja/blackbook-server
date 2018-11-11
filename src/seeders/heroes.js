4f6a02d939cd5bb29eda2ec7e752e5c53ed84f0b
Ghalib Nugroho <gholibinf25@gmail.com>
2018-11-11 11:29:11 +0700

    7 hero tambahan (nanti ditambah lagi)

--- src/seeders/heroes.js
+++ src/seeders/heroes.js
@@ -49,59 +49,59 @@ export default [
   },
   {
     'id': '7',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Omniknight',
+    'bio': `Purist Thunderwrath was a hard-fighting, road-worn, deeply committed knight, sworn to the order in which he had grown up as squire to elder knights of great reputation. He had spent his entire life in the service of the Omniscience, the All Seeing One. Theirs was a holy struggle, and so embedded was he in his duty that he never questioned it so long as he had the strength to fight and the impetuous valor that comes with youth. But over the long years of the crusade, as his elders passed away and were buried in sorry graves at the side of muddy tracks, as his bond-brothers fell in battle to uncouth creatures that refused to bow to the Omniscience, as his own squires were chewed away by ambush and plague and bad water, he began to question the meaning of his vows--the meaning of the whole crusade. After deep meditation, he parted ways with his army and commenced a long trek back to the cave-riddled cliffs of Emauracus, and there he set a challenge to the priests of the Omniscience. No knight had ever questioned them before, and they tried to throw him into the pit of sacrifice, but Purist would not be moved. For as he faced them down, he began to glow with a holy light, and they saw that the Omniscience had chosen to reveal Itself to him. The Elder Hierophant led him on a journey of weeks down into the deepest chamber, the holy of holies, where waited not some abstract concept of wisdom and insight, not some carved relic requiring an injection of imagination to believe in, but the old one itself. It had not merely dwelt in those rocks for billions of aeons; no, It had created them. The Omniscience had formed the immense mineral shell of the planet around itself, as a defense against the numerous terrors of space. Thus the All Seeing One claimed to have created the world, and given the other truths revealed to Purist on that day, the knight had no reason to refute the story. Perhaps the Omniscience is a liar, deep in its prison of stone, and not the world's creator at all, but Omniknight never again questioned his faith. His campaign had meaning at last. And there can be no question that the glorious powers that imbue him, and give his companions such strength in battle, are real beyond any doubt.`,
+    'tips_desc': 'Pro Omniknight offlane with suit',
+    'tips_video_url': 'https://www.youtube.com/watch?v=CHzDEmJBDoQ',
+    'image_url': 'omniknight_full.png'
   },
   {
     'id': '8',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Sven',
+    'bio': `Sven is the bastard son of a Vigil Knight, born of a Pallid Meranth, raised in the Shadeshore Ruins. With his father executed for violating the Vigil Codex, and his mother shunned by her wild race, Sven believes that honor is to be found in no social order, but only in himself. After tending his mother through a lingering death, he offered himself as a novice to the Vigil Knights, never revealing his identity. For thirteen years he studied in his father's school, mastering the rigid code that declared his existence an abomination. Then, on the day that should have been his In-Swearing, he seized the Outcast Blade, shattered the Sacred Helm, and burned the Codex in the Vigil's Holy Flame. He strode from Vigil Keep, forever solitary, following his private code to the last strict rune. Still a knight, yes...but a Rogue Knight. He answers only to himself.`,
+    'tips_desc': 'Check this out, miracle play as sven',
+    'tips_video_url': 'https://www.youtube.com/watch?v=BEE1tS5nJ48',
+    'image_url': 'sven_full.png'
   },
   {
     'id': '9',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Tiny',
+    'bio': `Coming to life as a chunk of stone, Tiny's origins are a mystery on which he continually speculates. He is a Stone Giant now, but what did he used to be? A splinter broken from a Golem's heel? A shard swept from a gargoyle-sculptor's workshop? A fragment of the Oracular Visage of Garthos? A deep curiosity drives him, and he travels the world tirelessly seeking his origins, his parentage, his people. As he roams, he gathers weight and size; the forces that weather lesser rocks, instead cause Tiny to grow and ever grow.`,
+    'tips_desc': 'Tiny 7800 MMR play',
+    'tips_video_url': 'https://www.youtube.com/watch?v=d7Kb3Vs9ZbQ',
+    'image_url': 'tiny_full.png'
   },
   {
     'id': '10',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Wraith King',
+    'bio': `For untold years, King Ostarion built a kingdom from the remains of his enemies. It was an obsessive's errand, done to pass the long eternities of a monarchy that seemed fated never to end. He believed that as long as he built up the towers of his palace, he could not die. But eventually he learned that he had been deluded... that bone itself could perish. Deeply mistrustful of flesh, he sought a more permanent way of extending his reign, and at last settled on pursuit of wraith energy, a form of pure spirit given off by certain dark souls at death. Should he infuse himself with Wraith Essence, he thought he might create a body as luminous and eternal as his ego. On the millennial solstice known as Wraith-Night, he submitted to a rite of transformation, compelling his subjects to harvest enough souls to fuel his ambition for immortality. No one knows how many of his champions died, for the only survivor who mattered was the Wraith King who rose with the sun on the following morn. Now he rarely spends a moment on his glowing throne--but strides out with sword drawn, demanding a fealty that extends far beyond death.`,
+    'tips_desc': 'Miracle pro dota 9k MMR',
+    'tips_video_url': 'https://www.youtube.com/watch?v=QfcWLLoIUUE',
+    'image_url': 'wraith_king_full.png'
   },
   {
     'id': '11',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Anti-Mage',
+    'bio': `The monks of Turstarkuri watched the rugged valleys below their mountain monastery as wave after wave of invaders swept through the lower kingdoms. Ascetic and pragmatic, in their remote monastic eyrie they remained aloof from mundane strife, wrapped in meditation that knew no gods or elements of magic. Then came the Legion of the Dead God, crusaders with a sinister mandate to replace all local worship with their Unliving Lord's poisonous nihilosophy. From a landscape that had known nothing but blood and battle for a thousand years, they tore the souls and bones of countless fallen legions and pitched them against Turstarkuri. The monastery stood scarcely a fortnight against the assault, and the few monks who bothered to surface from their meditations believed the invaders were but demonic visions sent to distract them from meditation. They died where they sat on their silken cushions. Only one youth survived--a pilgrim who had come as an acolyte, seeking wisdom, but had yet to be admitted to the monastery. He watched in horror as the monks to whom he had served tea and nettles were first slaughtered, then raised to join the ranks of the Dead God's priesthood. With nothing but a few of Turstarkuri's prized dogmatic scrolls, he crept away to the comparative safety of other lands, swearing to obliterate not only the Dead God's magic users--but to put an end to magic altogether.`,
+    'tips_desc': 'AM Pro Gameplay Arteezy',
+    'tips_video_url': 'https://www.youtube.com/watch?v=bjecgnQ6t-c',
+    'image_url': 'antimage_full.png'
   },
   {
     'id': '12',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Drow Ranger',
+    'bio': `Drow Ranger's given name is Traxex--a name well suited to the short, trollish, rather repulsive Drow people. But Traxex herself is not a Drow. Her parents were travelers in a caravan set upon by bandits, whose noisy slaughter of innocents roused the ire of the quiet Drow people. After the battle settled, the Drow discovered a small girl-child hiding in the ruined wagons, and agreed she could not be abandoned. Even as child, Traxex showed herself naturally adept at the arts they prized: Stealth, silence, subtlety. In spirit, if not in physique, she might have been a Drow changeling, returned to her proper home. But as she grew, she towered above her family and came to think of herself as ugly. After all, her features were smooth and symmetrical, entirely devoid of warts and coarse whiskers. Estranged from her adopted tribe, she withdrew to live alone in the woods. Lost travelers who find their way from the forest sometimes speak of an impossibly beautiful Ranger who peered at them from deep among the trees, then vanished like a dream before they could approach. Lithe and stealthy, icy hot, she moves like mist in silence. That whispering you hear is her frozen arrows finding an enemy's heart. `,
+    'tips_desc': 'Drow Ranger 20 kills 1 death',
+    'tips_video_url': 'https://www.youtube.com/watch?v=41gtVZo8MK4',
+    'image_url': 'drow_ranger_full.png'
   },
   {
     'id': '13',
-    'name': '',
-    'bio': ``,
-    'tips_desc': '',
-    'tips_video_url': '',
-    'image_url': ''
+    'name': 'Juggernaut',
+    'bio': `No one has ever seen the face hidden beneath the mask of Yurnero the Juggernaut. It is only speculation that he even has one. For defying a corrupt lord, Yurnero was exiled from the ancient Isle of Masks--a punishment that saved his life. The isle soon after vanished beneath the waves in a night of vengeful magic. He alone remains to carry on the Isle's long Juggernaut tradition, one of ritual and swordplay. The last practitioner of the art, Yurnero's confidence and courage are the result of endless practice; his inventive bladework proves that he has never stopped challenging himself. Still, his motives are as unreadable as his expression. For a hero who has lost everything twice over, he fights as if victory is a foregone conclusion.`,
+    'tips_desc': 'Pro Juggernaut Top 1 MMR',
+    'tips_video_url': 'https://www.youtube.com/watch?v=xYWv2SSPPjw',
+    'image_url': 'juggernaut_full.png'
   },
   {
     'id': '14',
